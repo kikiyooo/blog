5a588f2f9427328c327b8c670e69d6f18464cb2c
kikiyooo <kikiyo.mao@qq.com>
2024-04-23 00:36:27 +0800

    配置修改

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Kikiyo_Blog",
   description: "A VitePress Site",
+  base: "/blog/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
